cf4bd1d4d43d1ce37df3e8df83b31013ff1b7e98
ITV-107 <91462052+ITV-107@users.noreply.github.com>
2024-03-01 17:53:04 +0000

    Added comments

--- stress.sh
+++ stress.sh
@@ -1,8 +1,22 @@
-printf '\n\nMinimal stress test - rate of 10 and number of connections = 500\n'
-httperf --server $1 --rate 10 --num-conns=500 --port=8081
+#! /bin/bash
 
-printf '\n\nMedium stress test - rate of 500 and number of connections = 3000\n'
-httperf --server $1 --rate 500 --num-conns=3000 --port=8081
+# Runs HTTPERF.
+# Uses the --hog option for serious testing
+run_httperf() {
+	echo -e "\n\n- Rate of $2 packets/s and number of connections = $3\n"
+	httperf --hog --server $1 --rate $2 --num-conns=$3 --port=8081
+}
 
-printf '\n\nMaximum stress test - rate of 3000 and number of connections = 27000\n'
-httperf --server $1 --rate 3000 --num-conns=27000 --port=8081
+
+# Runs HTTPERF multiple times, incrementing the rate by 100 each time.
+iter_httperf() {
+	for i in {100..3000..100}
+	do
+		run_httperf $1 $i $2
+	done
+}
+
+# iterate over httperf using a set of simultaneous connections.
+iter_httperf $1 6250
+iter_httperf $1 12500
+iter_httperf $1 25000
